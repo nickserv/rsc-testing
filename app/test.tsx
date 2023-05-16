6ec9dd776dc61dd864c2cad90d42041879257940
Nick McCurdy <nick@nickmccurdy.com>
2023-05-15 22:32:45 -0400

    Initial implementation of `serverRender` function

--- app/test.tsx
+++ app/test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 import Layout from './layout'
 import Page from './page'
+import serverRender from './serverRender'
 
-beforeEach(() => render(<Page />, { wrapper: Layout }))
+beforeEach(() => serverRender(<Page />, { wrapper: Layout }))
 
 test.each(['client', 'server', 'layout'])(`%s component`, (keyword) => {
   expect(screen.getByText(keyword)).toBeInTheDocument()
