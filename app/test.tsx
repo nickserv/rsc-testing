8343ae20017003ba3bc9422c616e2ace2f84b20f
Nick McCurdy <nick@nickmccurdy.com>
2023-05-31 21:18:19 -0400

    Use Testing Library code style

--- app/test.tsx
+++ app/test.tsx
@@ -1,16 +1,16 @@
-import { render, screen, waitFor } from '@testing-library/react'
-import { Suspense } from 'react'
+import {render, screen, waitFor} from '@testing-library/react'
+import {Suspense} from 'react'
 import Layout from './layout'
 import Page from './page'
 
-test.each(['client', 'server', 'layout'])(`%s component`, async (keyword) => {
+test.each(['client', 'server', 'layout'])(`%s component`, async keyword => {
   render(
     <Suspense>
       <Layout>
         {/* @ts-expect-error Needs https://github.com/DefinitelyTyped/DefinitelyTyped/pull/65135 */}
         <Page />
       </Layout>
-    </Suspense>
+    </Suspense>,
   )
 
   await waitFor(() => expect(screen.getByText(keyword)).toBeInTheDocument())
