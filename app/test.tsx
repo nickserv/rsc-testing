a3cf88aa1ef919037148cfe80a8c0b6c74ce9055
Nick McCurdy <nick@nickmccurdy.com>
2023-09-10 19:19:56 -0400

    Remove unnecessary backtick string

--- app/test.tsx
+++ app/test.tsx
@@ -3,7 +3,7 @@ import { Suspense } from "react";
 import Layout from "./layout";
 import Page from "./page";
 
-test.each(["client", "server", "layout"])(`%s component`, async (keyword) => {
+test.each(["client", "server", "layout"])("%s component", async (keyword) => {
 	render(
 		<Suspense>
 			<Layout>
