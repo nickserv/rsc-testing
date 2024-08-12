62f6ab637f501e2a44c7f02cdb3536fa39919ee9
Abel Castro <hi@abelcastro.dev>
2024-08-12 21:25:32 +0200

    Workaround for using snapshot with async rsc and vitest
    I discovered what I believe is an issue when using snapshots with Vitest and React server components.
    The snapshots were being generated as an empty `<div />` tag.
    My workaround was to first perform some `screen` assertions (as shown in the test cases).
    After doing this, the snapshots are generated correctly when using `toMatchSnapshot`.

--- app/test.tsx
+++ app/test.tsx
@@ -17,3 +17,37 @@ test.each(["client", "server", "layout"])("%s component", async (keyword) => {
 
 	await screen.findByText(keyword);
 });
+
+test("Having <Layout> here will generate an empty snapshot", async () => {
+	const { container } = render(
+		<Suspense>
+			<Layout>
+				<Page />
+			</Layout>
+		</Suspense>,
+	);
+	await screen.findByText("server");
+	expect(container).toMatchSnapshot();
+});
+
+test("Don't using screen here will generate an empty snapshot", async () => {
+	const { container } = render(
+		<Suspense>
+			<Layout>
+				<Page />
+			</Layout>
+		</Suspense>,
+	);
+
+	expect(container).toMatchSnapshot();
+});
+
+test("Removing the <Layout> tag and calling screen generate a snapshot", async () => {
+	const { container } = render(
+		<Suspense>
+			<Page />
+		</Suspense>,
+	);
+	await screen.findByText("server");
+	expect(container).toMatchSnapshot();
+});
